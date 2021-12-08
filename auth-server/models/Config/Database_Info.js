b953b4caa633ce7ccfe5fe65a3dc6d73c338deac
Rodolfo Silva <35670736+Th0l@users.noreply.github.com>
2021-12-08 17:28:07 +0000

    edit

---
+++ auth-server/models/Config/Database_Info.js
@@ -0,0 +1,52 @@
+const connection = {
+
+    dbName: 'PI', //dbname here
+    options: {
+        host: 'localhost',
+        dialect: 'mysql',
+    },
+    user: {
+        username: 'root',
+        password: 'th0ldana' //your pass here
+    }
+};
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(connection.dbName, connection.user.username, connection.user.password, connection.options);
+
+module.exports = {
+    connection,
+    startConnection,
+    Sequelize: Sequelize,
+    sequelize: sequelize
+};
+
+function startConnection() {
+        
+    //Autenticação no SQL para ter a certeza que ligação foi feita!
+    sequelize.authenticate().then( () => {
+        console.log("Connected to the DB")
+    }).catch((err) => {
+        console.log("Connection Failed. " + err)
+    })
+
+    /*
+    const conn = new Sequelize(
+        connection.dbName,
+        connection.user.username,
+        connection.user.password,
+        connection.options
+    )
+    conn.authenticate((err, client) => {
+        if (err) return console.log(err);
+
+        console.log(client)
+        module.exports.conn = client;
+
+        return;
+    })*/
+};
+
+function simpleExecute() {
+
+}
