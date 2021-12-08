594c46d627bf66a8de3a838d97b34acdfabe9379
Rodolfo Silva <35670736+Th0l@users.noreply.github.com>
2021-12-08 17:20:30 +0000

    add

--- auth-server/routes/users.js
+++ auth-server/routes/users.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send("Empty location")
+  res.send('respond with a resource');
 });
-
-router.post('/login', (req,res,next) => {
-    res.send("Login area")
-})
 
 module.exports = router;
