eb8dfe78d7b51aabdc963c4545886342abd67882
13caroline <a80142@alunos.uminho.pt>
2021-12-11 17:30:53 +0000

    added router and views

---
+++ Frontend/web/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//import store from '../store'
+
+Vue.use(VueRouter)
+
+const routes = [
+  //------------------------------------- Cliente -------------------------------------
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/HomePage.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
+})
+
+export default router
---
+++ Frontend/web/src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="page">
+    <HomePage/>
+  </div>
+</template>
+
+<script>
+import HomePage from '@/components/HomePage.vue'
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    HomePage
+  },
+ 
+};
+</script>
