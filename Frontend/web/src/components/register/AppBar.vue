4d29e418bbe38bd20655b59304e5f0d00fb8a024
João Mota <joaodiogomota12@gmail.com>
2021-12-16 17:51:42 +0000

    AppBar register

--- Frontend/web/src/components/register/AppBar.vue
+++ Frontend/web/src/components/register/AppBar.vue
@@ -1,39 +1,19 @@
 <template>
   <div>
-    <v-app-bar flat color="#FFFFFF" height="180">
-      <v-container>
-        <v-layout row>
-          <v-col class="d-flex justify-space-around">
-            <v-img
-              align-center
-              center
-              src="@/assets/logo.png"
-              max-height="220"
-              max-width="220"
-            >
-            </v-img>
-          </v-col>
-          <v-col>
-               <v-btn
-                outlined 
-                color="#78C4D4"
-                depressed
-                class="rounded-xl"
-            >
-             Iniciar Sessão
-            </v-btn>
-          </v-col>
-        </v-layout>
-        
-           
-       
-      </v-container>
+    <v-app-bar flat color="#FFFFFF" height="90">
+      <v-spacer></v-spacer>
+      <v-img src="@/assets/logo.png" max-height="270" max-width="270" class="mt-12"> </v-img>
+
+      <v-spacer></v-spacer>
+
+      <v-btn outlined color="#78C4D4" depressed class="rounded-xl">
+        Iniciar Sessão
+      </v-btn>
     </v-app-bar>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "appbar",
   data() {
