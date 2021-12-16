7adb4569a4ce53858942bef84785802e7741f0b5
Bruno Veloso <brunocveloso7@gmail.com>
2021-12-16 15:02:05 +0000

    update scripts

---
+++ Database/povoamento/area_category.sql
@@ -0,0 +1,9 @@
+USE PI;
+
+INSERT INTO Area(idArea,name)VALUES(1,'Apoio a idosos');
+INSERT INTO Category(idCategory,name,idArea)VALUES(1,'Compras',1);
+INSERT INTO Category(idCategory,name,idArea)VALUES(2,'Passeios',1);
+INSERT INTO Category(idCategory,name,idArea)VALUES(3,'Refeições',1);
+INSERT INTO Category(idCategory,name,idArea)VALUES(4,'Medicação',1);
+INSERT INTO Category(idCategory,name,idArea)VALUES(5,'Higiene',1);
+INSERT INTO Category(idCategory,name,idArea)VALUES(6,'Companhia',1);
