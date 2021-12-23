c4fae0b6e4e303c744c45fc232eea12380aa457d
luisabreu102030 <36260657+luisabreu102030@users.noreply.github.com>
2021-12-23 22:17:58 +0000

    Update AppBar
    AppBar presents the list of options, like: login, help, etc

--- Frontend/web/src/components/register/AppBar.vue
+++ Frontend/web/src/components/register/AppBar.vue
@@ -12,18 +12,20 @@
           </v-btn>
           <v-app-bar-nav-icon outlined color="#78C4D4" class="rounded-xl hidden-sm-and-up" @click.stop="drawer = !drawer">          </v-app-bar-nav-icon>
           
-          <v-navigation-drawer app v-model="drawer" absolute temporary right>
+          <v-navigation-drawer app v-model="drawer" absolute overlay-opacity="100" temporary right >
             
             <v-list dense nav>  
-              <v-list-item>
+              <v-list-item v-for="(item, i) in items" :key="i" @click="menuActionClick(item.action)">
                 <v-list-item-icon>
-                  <v-icon>mdi-view-dashboard</v-icon>
+                  <v-icon v-text="item.icon"></v-icon>
                 </v-list-item-icon>
 
                 <v-list-item-content>
-                  <v-list-item-title @click="login()">Iniciar Sessão</v-list-item-title>
+                  <v-list-item-title v-text="item.text"></v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
+            
+            
             </v-list>
 
           </v-navigation-drawer>
@@ -41,13 +43,24 @@ export default {
   data() {
     return {
       drawer: null,
+      items: [
+        { text: 'Iniciar Sessão', icon: 'mdi-view-dashboard', action: "login" },
+        { text: 'Ajuda', icon: 'mdi-view-dashboard', action: null },
+      ],
       
     };
   },
   methods: {
     login() {
       this.$router.push("/");
+    },
+
+    menuActionClick(action) {
+      if (action === "login") {
+        this.login()
+      } 
     }
+
   }
 };
 </script>
