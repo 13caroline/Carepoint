721b0582f21f033c03d1dd11a8d8a43c780a1984
João Mota <joaodiogomota12@gmail.com>
2021-12-24 17:31:49 +0000

    updates profile

--- Frontend/web/src/views/global/Profile.vue
+++ Frontend/web/src/views/global/Profile.vue
@@ -3,8 +3,7 @@
     <Bar />
     <v-container>
       <v-layout row wrap justify-center align-center>
-        
-          <v-flex xs4 md3>
+        <v-flex xs4 md3>
           <v-col cols="12" md="auto">
             <div class="foto mx-auto">
               <v-img
@@ -26,13 +25,15 @@
                 </template>
               </v-img>
             </div>
-            <v-btn outline>Editar Imagem</v-btn>
+            <v-btn raised rounded class="editImg" color="#78C4D4" outlined @click="onPick()"> Alterar fotografia </v-btn>
+            <input type="file" class="editImg" style="display: none" ref="fileInput" accept="image/*">
+            <!--<v-btn  color="#78C4D4" >Alterar fotografia</v-btn>-->
           </v-col>
-          </v-flex>
-          <v-flex xs4 md5>
+        </v-flex>
+        <v-flex xs4 md5>
           <v-col>
             <h3>Dados pessoais</h3>
-            <v-card  class="dadosCard" outlined >
+            <v-card  class="dadosCard" outlined>
               <v-list-item class="my-auto">
                 <v-list-item-content>
                   <div>
@@ -50,16 +51,16 @@
                       <v-col justify-end>
                         <p class="infos">Telemóvel</p>
                       </v-col>
-                      <v-col >
+                      <v-col>
                         <p class="respos">918273339</p>
                       </v-col>
                     </v-row>
                   </div>
                   <div>
                     <v-row>
-                      <v-col >
+                      <v-col>
                         <p class="infos">Sexo</p>
-                      </v-col>  
+                      </v-col>
                       <v-col>
                         <p class="respos">Feminino</p>
                       </v-col>
@@ -67,7 +68,7 @@
                   </div>
                   <div>
                     <v-row>
-                      <v-col >
+                      <v-col>
                         <p class="infos">Localidade</p>
                       </v-col>
                       <v-col>
@@ -77,9 +78,10 @@
                   </div>
                 </v-list-item-content>
               </v-list-item>
+                <v-icon class="editIcon" @click="doesSomething()">mdi-pencil</v-icon>
             </v-card>
             <h3>Dados de acesso</h3>
-            <v-card  class="dadosCard" outlined>
+            <v-card class="dadosCard" outlined>
               <v-list-item class="my-auto">
                 <v-list-item-content>
                   <div>
@@ -97,27 +99,22 @@
                       <v-col justify-end>
                         <p class="infos">Password</p>
                       </v-col>
-                      <v-col >
+                      <v-col>
                         <p class="respos">pass</p>
                       </v-col>
                     </v-row>
                   </div>
-                  
                 </v-list-item-content>
               </v-list-item>
             </v-card>
-
-            
           </v-col>
-          </v-flex>
+        </v-flex>
       </v-layout>
       <v-layout row wrap justify-center align-center>
-       <v-row>
-         <v-flex xs4 md3>
-          
-          </v-flex>
+        <v-row>
+          <v-flex xs4 md3> </v-flex>
         </v-row>
-        </v-layout>
+      </v-layout>
     </v-container>
 
     <Foot />
@@ -127,13 +124,20 @@
 <script>
 export default {
   data() {
-    return {
-    };
+    return {};
   },
   components: {
     Bar: () => import("@/components/global/AppBarAccount.vue"),
     Foot: () => import("@/components/global/Footer"),
   },
+  methods: {
+    doesSomething(){
+      console.log("Does something")
+    },
+    onPick(){
+      this.$refs.fileInput.click()
+    }
+  }
 };
 </script>
 
@@ -142,18 +146,23 @@ export default {
   outline-color: black;
 }
 
-
 span {
   font-weight: bold;
 }
 
-.dadosCard{
+.dadosCard {
   margin-bottom: 5em;
-  
 }
 
-.respos{
+.respos {
   font-weight: bold;
+}
+
+.editImg{
+  margin-top: 10%;
+  margin-left:11%;
+  border:  solid #78C4D4;
+
 }
 </style>
 
