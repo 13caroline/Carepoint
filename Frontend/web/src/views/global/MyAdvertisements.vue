712c5683b5537fcfb0f52e43a352f554f2e9168e
João Mota <joaodiogomota12@gmail.com>
2021-12-29 20:04:07 +0000

    My Advertisements setup page

---
+++ Frontend/web/src/views/global/MyAdvertisements.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Bar/>
+        
+        <Foot/>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Bar: () => import("@/components/global/AppBarAccount.vue"),
+    Foot: () => import("@/components/global/Footer"),
+  },
+
+  methods: {
+  },
+};
+</script>
