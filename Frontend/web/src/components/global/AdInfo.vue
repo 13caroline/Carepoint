4242ad7aa228c82c445fd6f8e3011ae998c1d48e
João Mota <joaodiogomota12@gmail.com>
2022-01-02 21:56:37 +0000

    Update SP from consumer

--- Frontend/web/src/components/global/AdInfo.vue
+++ Frontend/web/src/components/global/AdInfo.vue
@@ -26,7 +26,7 @@
           <v-list-item>
             <v-list-item-content>
               <div>
-                <p class="infos font-weight-bold">Nome completo do prestador</p>
+                <p class="infos font-weight-bold">{{ serviceProvider.name }}</p>
               </div>
               <div>
                 <p class="infos font-weight-bold">Anos de experiência</p>
@@ -77,37 +77,66 @@
     <v-row class="w-100 ma-0">
       <v-col cols="12" md="4" sm="4">
         <p class="infos font-weight-bold">Contactos</p>
-        <div class="infos">+351 9151212141</div>
-        <div class="infos">exemplo@exemplo.com</div>
+        <div class="infos">{{ serviceProvider.phoneNumber }}</div>
+        <div class="infos">{{ serviceProvider.email }}</div>
       </v-col>
 
       <v-col cols="12" md="8" sm="8">
         <p class="infos font-weight-bold">Horário</p>
-        <VueSchedule
+        <!-- <VueSchedule
           v-model="schedule"
           bg="white"
             bgHover="gray"
             bgActive="#c0e4ec"
-        />
+        />-->
       </v-col>
     </v-row>
 
-    <v-row class="w-100 ma-0">
-      <v-col cols="12" md="4" sm="4">
-        <p class="infos font-weight-bold">Comentários</p>
-      </v-col>
+    <v-row
+      class="w-100 ma-0"
+      v-for="(a, index) in serviceProvider.reviews"
+      :key="index"
+    >
+      <v-card
+        class="card pa-5 rounded-xl overflow-auto"
+        outlined
+        tile
+        :style="styleObject"
+        width="100%"
+        to="/ad/info"
+      >
+      <v-row align="center">
+        <v-col cols="12" md="11" sm="11">
+          <div>
+            <span class="font-weight-bold">{{ a.postDate }}</span>
+          </div>
+          <div justify="center" class="mx-auto">
+            <span class="infos ">
+              {{ a.description }}
+            </span>
+          </div>
+        </v-col>
+        <v-col cols="12" md="1" sm="1" >
+            <span class="classification">{{ a.rating }}</span> /10
+        
+        </v-col>
+        </v-row>
+      </v-card>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import 'vue-daily-scheduler/dist/vue-schedule.min.css'
-import VueSchedule from "vue-daily-scheduler";
+import "vue-daily-scheduler/dist/vue-schedule.min.css";
+//import VueSchedule from "vue-daily-scheduler";
+import axios from "axios";
+
 export default {
   name: "Ads",
 
   data() {
     return {
+      styleObject: { border: "1px solid #78c4d4" },
       category: ["Companhia", "Compras", "Medicação", "Higiene"],
       schedule: {
         0: [],
@@ -118,10 +147,41 @@ export default {
         5: [],
         6: [],
       },
+      serviceProvider: {
+        reviews: [
+          {
+            postDate: "12/12/2020",
+            description:
+              "Isto é uma review da pessoa. Muito agradavel, fez tudo o pedido e por um preço acessível, recomendo 100%",
+            rating: 9.5,
+          },
+        ],
+      },
     };
   },
   components: {
-    VueSchedule,
+    // VueSchedule,
+  },
+
+  created: async function () {
+    try {
+      let response = await axios.get(
+        "http://localhost:9040/serviceProvider/51",
+        {
+          //id: this.id,
+        }
+        //{ headers: { Authorization: "Bearer " + store.getters.token } }
+      );
+      console.log(response);
+      //this.serviceProvider = response.data;
+    } catch (e) {
+      this.$snackbar.showMessage({
+        show: true,
+        color: "error",
+        text: "Ocorreu um erro. Por favor tente mais tarde!",
+        timeout: 4000,
+      });
+    }
   },
 };
 </script>
