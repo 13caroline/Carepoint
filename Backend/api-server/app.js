2bd8bc39d46a2cf688bc1148d035f6b362d23069
Rodolfo Silva <35670736+Th0l@users.noreply.github.com>
2022-01-02 14:37:54 +0000

    CORS FIX

--- Backend/api-server/app.js
+++ Backend/api-server/app.js
@@ -50,6 +50,25 @@ app.use('/add', addRouter);
 app.use('/review', reviewRouter);
 
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
