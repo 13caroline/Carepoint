7ac415bf0b2a9fb3411ff7dc117cffa5fe3fde8a
João Mota <joaodiogomota12@gmail.com>
2022-01-17 21:09:44 +0000

    resend job offer

--- Frontend/web/src/views/consumer/MyAdvertisements.vue
+++ Frontend/web/src/views/consumer/MyAdvertisements.vue
@@ -22,7 +22,6 @@
         :items="ads"
         :items-per-page.sync="itemsPerPage"
         :page.sync="page"
-        :sort-desc="sortDesc"
         hide-default-footer
         @page-count="pageCount == $event"
         no-data-text="Não existem anúncios publicados."
@@ -94,6 +93,23 @@
 
                   <v-tooltip top>
                     <template v-slot:activator="{ on, attrs }">
+                      <v-btn icon @click="openDialog(a)">
+                        <v-icon
+                          color="#B2DFDB"
+                          dark
+                          v-bind="attrs"
+                          small
+                          v-on="on"
+                        >
+                          fas fa-redo
+                        </v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Republicar anúncio</span>
+                  </v-tooltip>
+
+                  <v-tooltip top>
+                    <template v-slot:activator="{ on, attrs }">
                       <v-btn
                         icon
                         @click="conclude(a.idJobOffer)"
@@ -139,35 +155,162 @@
       </v-data-iterator>
       <small v-else> <em> não existem anúncios publicados </em></small>
 
-    <v-row class="mt-4" align="center" justify="center" v-if="ads.length">
-      <v-btn
-        fab
-        dark
-        x-small
-        depressed
-        color="#78C4D4"
-        class="mr-1"
-        @click="formerPage"
-      >
-        <v-icon>mdi-chevron-left</v-icon>
-      </v-btn>
-      <v-btn
-        fab
-        dark
-        x-small
-        depressed
-        color="#78C4D4"
-        class="ml-1"
-        @click="nextPage"
-      >
-        <v-icon>mdi-chevron-right</v-icon>
-      </v-btn>
-    </v-row>
+      <v-row class="mt-4" align="center" justify="center" v-if="ads.length">
+        <v-btn
+          fab
+          dark
+          x-small
+          depressed
+          color="#78C4D4"
+          class="mr-1"
+          @click="formerPage"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          dark
+          x-small
+          depressed
+          color="#78C4D4"
+          class="ml-1"
+          @click="nextPage"
+        >
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-row>
 
-    <v-row class="mt-5" align="center" justify="center" v-if="ads.length">
-      <span class="grey--text">Página {{ page }} de {{ numberOfPages }}</span>
-    </v-row>
+      <v-row class="mt-5" align="center" justify="center" v-if="ads.length">
+        <span class="grey--text">Página {{ page }} de {{ numberOfPages }}</span>
+      </v-row>
     </v-container>
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2 mb-3">
+          Repetir anúncio de trabalho
+        </v-card-title>
+
+        <v-card-text>
+          <v-row>
+            <v-col class="pb-0" align="right" cols="5">
+              <span class="text-uppercase">Categoria</span>
+            </v-col>
+            <v-col class="pl-0 pb-0" cols="7">
+              <span class="black--text">
+                <strong> {{ dialogData.categoryName }} </strong>
+              </span>
+              <br />
+              <span> Apoio a idosos </span>
+            </v-col>
+
+            <v-col class="pb-0" align="right" cols="5">
+              <span class="text-uppercase">Localização</span>
+            </v-col>
+            <v-col class="pl-0 pb-0" cols="7">
+              <span class="black--text">
+                <strong>{{ dialogData.locationName }}</strong>
+              </span>
+            </v-col>
+
+            <v-col class="pb-0" align="right" cols="5">
+              <span class="text-uppercase">Valor</span>
+            </v-col>
+            <v-col class="pl-0 pb-0" cols="7">
+              <span class="black--text">
+                <strong>{{ dialogData.price }} €/hora</strong>
+              </span>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12" md="6">
+              <span>Data de início *</span>
+              <v-menu
+                v-model="menu"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="dateBegin"
+                    append-icon="fas fa-calendar-alt"
+                    readonly
+                    dense
+                    outlined
+                    required
+                    color="#78c4d4"
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="dateBegin"
+                  color="#78c4d4"
+                  @input="menu = false"
+                  locale="pt PT"
+                  :min="new Date().toISOString().substr(0, 10)"
+                ></v-date-picker>
+              </v-menu>
+            </v-col>
+
+            <v-col cols="12" md="6">
+              <span>Data de término *</span>
+              <v-menu
+                v-model="menu2"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="dateEnd"
+                    append-icon="fas fa-calendar-alt"
+                    readonly
+                    dense
+                    outlined
+                    required
+                    v-bind="attrs"
+                    color="#78c4d4"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="dateEnd"
+                  color="#78c4d4"
+                  @input="menu2 = false"
+                  locale="pt PT"
+                  :min="new Date().toISOString().substr(0, 10)"
+                ></v-date-picker>
+              </v-menu>
+            </v-col>
+          </v-row>
+           <v-tooltip top>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        icon
+                        @click="postAd()"
+                        
+                      >
+                        <v-icon color="#66BB6A" dark v-bind="attrs" v-on="on">
+                          mdi-calendar-check
+                        </v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Publicar anúncio</span>
+                  </v-tooltip>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <Foot />
   </div>
 </template>
@@ -178,6 +321,12 @@ import store from "@/store/index.js";
 export default {
   data() {
     return {
+      dateBegin: "",
+      dateEnd: "",
+      menu: false,
+      menu2: false,
+      dialog: false,
+      dialogData: {},
       show: false,
       ads: [],
       pageCount: 0,
@@ -208,6 +357,10 @@ export default {
       if (this.page - 1 >= 1) this.page -= 1;
       this.getData();
     },
+    openDialog(a) {
+      this.dialog = true;
+      this.dialogData = a;
+    },
     update: async function () {
       this.ads = [];
       try {
@@ -232,7 +385,6 @@ export default {
           id_job_offer: id,
         });
         this.update();
-        
       } catch (e) {
         this.$snackbar.showMessage({
           show: true,
@@ -241,6 +393,37 @@ export default {
           timeout: 4000,
         });
       }
+    },
+    postAd: async function () {
+     // if (this.$refs.form.validate()) {
+        console.log(this.dialogData)
+        try {
+          await axios.post("http://localhost:9040/joboffer/new", {
+            token: store.getters.token,
+            price: this.dialogData.price,
+            description: "DescTeste",
+            beginDate: this.dateBegin,
+            endDate: this.dateEnd,
+            idCategory: this.dialogData.categoryName,
+            idLocation: this.dialogData.locationName,
+          });
+          this.$router.push("/my/advertisements");
+          this.$snackbar.showMessage({
+            show: true,
+            color: "success",
+            text: "Anúncio publicado",
+            snackbar: true,
+            timeout: 4000,
+          });
+        } catch (e) {
+          this.$snackbar.showMessage({
+            show: true,
+            color: "error",
+            text: "Ocorreu um erro. Por favor tente mais tarde!",
+            timeout: 4000,
+          });
+        }
+     // }
     },
   },
   created: async function () {
