41cc8b96bf6e171b145e4ce1553f19ed1204b3c3
Bruno Veloso <brunocveloso7@gmail.com>
2022-01-19 15:12:05 +0000

    fixed trigger

--- Database/sql/procedure_perfil.sql
+++ Database/sql/procedure_perfil.sql
@@ -12,7 +12,15 @@ DROP PROCEDURE IF EXISTS get_company_profile;
 DROP PROCEDURE IF EXISTS get_user_image;
 DROP PROCEDURE IF EXISTS get_company_profile_2;
 
--- Returns: average rating or null if no reviews
+
+-- =============================================
+-- Description: Calculate the average rating of a service provider, based on reviews received
+-- Type: Procedure
+-- Parameters:
+--   @id - service provider identification number
+-- Returns: Average rating or null if the person doesn't have reviews
+-- =============================================
+
 DELIMITER &&  
 CREATE PROCEDURE get_average_rating (IN id INT)  
 BEGIN  
@@ -20,7 +28,14 @@ BEGIN
 END &&  
 DELIMITER ;
 
--- Returns: returns all info about categories that belongs to a specific SP
+-- =============================================
+-- Description: Get all information about categories of a scpecific service provider
+-- Type: Procedure
+-- Parameters:
+--   @id - service provider identification number
+-- Returns: All information about categories that belongs to a specific service provider (name of category, experience, work schedule and price)
+-- =============================================
+
 DELIMITER &&
 CREATE PROCEDURE get_sp_category_info (IN id INT)
 BEGIN
@@ -31,7 +46,14 @@ BEGIN
 END &&
 DELIMITER ;
 
--- Returns: returns all info about categories that belongs to a specific SP
+-- =============================================
+-- Description: Get all information about categories of a scpecific service provider, except work schedule
+-- Type: Procedure
+-- Parameters:
+--   @id - service provider identification number
+-- Returns: All information about categories that belongs to a specific service provider (name of category, experience and price), except work schedule
+-- =============================================
+
 DELIMITER &&
 CREATE PROCEDURE get_sp_only_category_info (IN id INT)
 BEGIN
@@ -41,7 +63,14 @@ BEGIN
 END &&
 DELIMITER ;
 
--- Returns: returns all info about categories that belongs to a specific SP
+-- =============================================
+-- Description: Get work schedule information of a scpecific service provider
+-- Type: Procedure
+-- Parameters:
+--   @id - service provider identification number
+-- Returns: Work schedule and category name that belongs to a specific service provider 
+-- =============================================
+
 DELIMITER &&
 CREATE PROCEDURE get_sp_horarios (IN id INT)
 BEGIN
@@ -51,7 +80,14 @@ BEGIN
 END &&
 DELIMITER ;
 
--- Returns: returns all the reviews for a specific user
+-- =============================================
+-- Description: Get all reviews for a certain service provider
+-- Type: Procedure
+-- Parameters:
+--   @id - service provider identification number
+-- Returns: All reviews for a certain service provider
+-- =============================================
+
 DELIMITER &&  
 CREATE PROCEDURE get_reviews (IN id INT)  
 BEGIN  
@@ -61,7 +97,14 @@ END &&
 DELIMITER ;  
 
 
--- Returns: returns information for a specific service provider
+-- =============================================
+-- Description: Get all information of a scpecific service provider, except categories information  
+-- Type: Procedure
+-- Parameters:
+--   @id - service provider identification number
+-- Returns: All information of a scpecific service provider, except categories information  
+-- =============================================
+
 DELIMITER &&  
 CREATE PROCEDURE get_service_provider_profile (IN id INT)  
 BEGIN  
@@ -73,7 +116,14 @@ BEGIN
 END &&  
 DELIMITER ; 
 
--- Returns: returns information for a specific consumer
+-- =============================================
+-- Description: Get all information of a scpecific consumer
+-- Type: Procedure
+-- Parameters:
+--   @em - consumer email
+-- Returns: All information of a scpecific consumer
+-- =============================================
+
 DELIMITER &&  
 CREATE PROCEDURE get_consumer_profile (IN em VARCHAR(90))
 BEGIN
@@ -83,7 +133,14 @@ BEGIN
 END &&
 DELIMITER ;
 
--- Returns: returns information for a specific service provider
+-- =============================================
+-- Description: Get all information of a scpecific service provider, including subscription information, except categories information  
+-- Type: Procedure
+-- Parameters:
+--   @em - service provider email
+-- Returns: All information of a scpecific service provider, including subscription information, except categories information  
+-- =============================================
+
 DELIMITER &&  
 CREATE PROCEDURE get_service_provider_profile_v2 (IN em VARCHAR(90))  
 BEGIN  
@@ -97,7 +154,14 @@ BEGIN
 END &&  
 DELIMITER ;
 
--- Returns: returns information for a specific company
+-- =============================================
+-- Description: Get all information of a scpecific company, including subscription information
+-- Type: Procedure
+-- Parameters:
+--   @em - Company email
+-- Returns: All information of a scpecific company, including subscription information
+-- =============================================
+
 DELIMITER &&
 CREATE PROCEDURE get_company_profile (IN em VARCHAR(90))
 BEGIN
@@ -111,7 +175,14 @@ BEGIN
 END &&
 DELIMITER ;
 
--- Returns: returns information for a specific company
+-- =============================================
+-- Description: Get all information of a scpecific company, except subscription information
+-- Type: Procedure
+-- Parameters:
+--   @id - Company identification number
+-- Returns: All information of a scpecific company, except subscription information
+-- =============================================
+
 DELIMITER &&
 CREATE PROCEDURE get_company_profile_2 (IN id INT)
 BEGIN
@@ -123,6 +194,14 @@ BEGIN
     INNER JOIN file ON user.idUser = file.idUser WHERE id = user.idUser;
 END &&
 DELIMITER ;
+
+-- =============================================
+-- Description: Get user profile picture
+-- Type: Procedure
+-- Parameters:
+--   @em - user email
+-- Returns: Profile picture of the user
+-- =============================================
 
 DELIMITER &&
 CREATE PROCEDURE get_user_image (IN em VARCHAR(90))
