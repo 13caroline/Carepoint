71b254e8846c001a0ae25d9f43eaebb966efaefe
Bruno Veloso <brunocveloso7@gmail.com>
2022-01-20 21:59:38 +0000

    create message

--- Database/sql/procedure_inserts.sql
+++ Database/sql/procedure_inserts.sql
@@ -1,6 +1,7 @@
 USE PI;
 
 DROP PROCEDURE IF EXISTS createReview;
+DROP PROCEDURE IF EXISTS createMessage;
 
 -- =============================================
 -- Description: Insert review
@@ -19,6 +20,26 @@ BEGIN
 
 	INSERT INTO review (description, rating, postDate, idGive, idReceive)
     VALUES (in_description, in_rating, now(), in_give, in_receive);
+        
+END &&  
+DELIMITER ;
+
+-- =============================================
+-- Description: Insert message
+-- Type: Procedure
+-- Parameters:
+--   @in_content -  content of the message
+--   @in_idGive -  person that gives the review
+--   @in_idReceive - person that gets the review
+-- Returns: Nothing
+-- =============================================
+
+DELIMITER &&  
+CREATE PROCEDURE createMessage (IN in_content VARCHAR(200), IN in_idGive INT, IN in_idReceive INT)  
+BEGIN  
+
+	INSERT INTO message (idMessage, date, content, idGive, idReceive)
+    VALUES (0, now(), in_content, in_idGive, in_idReceive);
         
 END &&  
 DELIMITER ;
