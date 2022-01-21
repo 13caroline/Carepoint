956f81f49c3b860ea8674c3c6a1a98de36220a00
Hugo <hugosf13@gmail.com>
2022-01-21 19:23:18 +0000

    Add Message

--- Backend/api-server/controllers/message.js
+++ Backend/api-server/controllers/message.js
@@ -3,7 +3,7 @@ const dbconfig = require('../models/Config/Database_Info')
 
 var Out = module.exports;
 
-//GET all users talked with by user ID
+//POST all users talked with by user ID
 Out.getAllId = (idUser) => {
     return dbconfig.sequelize.query('CALL user_messages_with (:in_idUser)',
     {replacements: {
@@ -11,12 +11,23 @@ Out.getAllId = (idUser) => {
     }});
 }
 
-//GET all messages between two users
+//POST all messages between two users
 
 Out.getAllMessagesBetween = (idUser1,idUser2) => {
     return dbconfig.sequelize.query('CALL all_messages_with (:in_idUser1, :in_idUser2)',
     {replacements: {
         in_idUser1: idUser1,
         in_idUser2: idUser2
+    }});
+}
+
+//PUT add a new message
+
+Out.addMessage = (content,idUser1,idUser2) => {
+    return dbconfig.sequelize.query('CALL createMessage (:in_content, :in_idGive, :in_idReceive)',
+    {replacements: {
+        in_content: content,
+        in_idGive: idUser1,
+        in_idReceive: idUser2
     }});
 }
--- Backend/api-server/routes/message.js
+++ Backend/api-server/routes/message.js
@@ -5,25 +5,39 @@ const auth = require('../authorization/auth')
 const message_controller = require('../controllers/message');
 const User = require('../controllers/user');
 
-router.get('/:id', function(req, res, next) {
-    console.log(req.body)
-    console.log(req.params.id)
-    message_controller.getAllId(req.params.id)
-    .then(data => res.status(200).jsonp(data))
-    .catch(e => res.status(500).jsonp({ error: e }))
-})
-
-router.get('/', auth.validToken, function(req, res, next) {
+router.post('/:id', function(req, res, next) {
     token = req.body.token;
     email = auth.getEmailFromJWT(token);
-    idUser2 = parseInt(req.body.idUser2)
+
+    User.consult(email).then((user) => {
+        message_controller.getAllId(user.idUser)
+        .then((dt) => res.status(200).jsonp(dt))
+        .catch((err) => res.status(500).jsonp({error: err}))
+    })
+})
+
+router.post('/', auth.validToken, function(req, res, next) {
+    token = req.body.token;
+    email = auth.getEmailFromJWT(token);
+    idUser2 = parseInt(req.body.idUser2);
 
     User.consult(email).then((user) => {
         message_controller.getAllMessagesBetween(user.idUser,idUser2)
         .then((dt) => res.status(200).jsonp(dt))
         .catch((err) => res.status(500).jsonp({error: err}))
     });
-
 })
 
+router.put('/', auth.validToken, function(req, res, next) {
+    token = req.body.token;
+    email = auth.getEmailFromJWT(token);
+    idUser2 = parseInt(req.body.idUser2);
+    console.log(req.body.content);
+
+    User.consult(email).then((user) => {
+        message_controller.addMessage(req.body.content,user.idUser,idUser2)
+        .then((dt) => res.status(200).jsonp(dt))
+        .catch((err) => res.status(500).jsonp({error: err}))
+    });
+})
 module.exports = router;
