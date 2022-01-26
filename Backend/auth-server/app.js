ecfbc26aef0fa8619b5e8b3f821601c64d78d32e
Rodolfo Silva <35670736+Th0l@users.noreply.github.com>
2022-01-26 14:10:40 +0000

    Update app.js

--- Backend/auth-server/app.js
+++ Backend/auth-server/app.js
@@ -35,6 +35,7 @@ app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9041');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
