46219968ca9ff1797982eb8829a536443561f2a9
João Mota <joaodiogomota12@gmail.com>
2022-01-30 23:07:08 +0000

    Messages update

--- Frontend/web/src/views/global/Messages.vue
+++ Frontend/web/src/views/global/Messages.vue
@@ -4,31 +4,29 @@
     <v-container>
       <v-row no-gutters class="w-100">
         <v-col cols="3">
-          <v-card class="mx-auto"  style="height: 80vh">
+          <v-card class="mx-auto" style="height: 67vh">
             <v-list subheader>
               <v-subheader>Prestadores de Serviços</v-subheader>
-                  <v-list-item-group
-        active-class="light-blue lighten-4"
-      >
-              <v-list-item
-                v-for="item in users"
-                :key="item.title"
-                @click="openChat(item)"
-              >
-                <v-list-item-avatar>
-                  <v-img :src="processImage(item.image)"></v-img>
-                </v-list-item-avatar>
-                <v-list-item-content>
-                  <v-list-item-title v-text="item.title"></v-list-item-title>
-                </v-list-item-content>
-                <v-list-item-icon>
-                  <v-icon
-                    :color="item.active ? 'deep-purple accent-4' : 'grey'"
-                    >{{ item.name }}</v-icon
-                  >
-                </v-list-item-icon>
-              </v-list-item>
-                  </v-list-item-group>
+              <v-list-item-group active-class="light-blue lighten-4">
+                <v-list-item
+                  v-for="item in users"
+                  :key="item.title"
+                  @click="openChat(item)"
+                >
+                  <v-list-item-avatar>
+                    <v-img :src="processImage(item.image)"></v-img>
+                  </v-list-item-avatar>
+                  <v-list-item-content>
+                    <v-list-item-title v-text="item.title"></v-list-item-title>
+                  </v-list-item-content>
+                  <v-list-item-icon>
+                    <v-icon
+                      :color="item.active ? 'deep-purple accent-4' : 'grey'"
+                      >{{ item.name }}</v-icon
+                    >
+                  </v-list-item-icon>
+                </v-list-item>
+              </v-list-item-group>
             </v-list>
             <!--<v-divider></v-divider>
              <v-list subheader>
@@ -49,11 +47,12 @@
           </v-card>
         </v-col>
 
-        <v-col cols="9">
-          <v-card class="mx-auto " style="height: 80vh">
+        <v-col cols="12" md="9">
+          <v-card>
+          <v-card class="mx-auto overflow-x-hidden bCard" style="height: 60vh" outlined color="transparent">
             <v-row justify="end">
               <v-col>
-                <v-list disabled rounded>
+                <v-list disabled rounded class="mb-5">
                   <v-list-item-group color="primary">
                     <v-list-item
                       v-for="(item, i) in messages"
@@ -61,19 +60,25 @@
                       :class="item.sent ? 'text-right' : ''"
                       class="text-right align-self-start"
                     >
-                      <v-avatar size="36" v-if="item.idReceive == idUser">
-                        <v-img :src="processImage(pic)"></v-img>
-                      </v-avatar>
-                      <v-chip
-                      class="pa-6"
-                        pill
-                        v-if="item.idReceive == idUser"
-                        color="#c0e4ec"
-                      >
-                        {{ item.content }}
-                      </v-chip>
+                      <div v-if="item.idReceive == idUser">
+                        <p class="overline mb-0">{{ item.date }}</p>
+                        <v-row class="mt-1">
+                          <v-avatar size="36">
+                            <v-img :src="processImage(pic)"></v-img>
+                          </v-avatar>
+                          <v-chip
+                            class="pa-6"
+                            pill
+                            v-if="item.idReceive == idUser"
+                            color="#c0e4ec"
+                          >
+                            {{ item.content }}
+                          </v-chip>
+                        </v-row>
+                      </div>
                       <v-row v-else>
                         <v-col>
+                          <p class="overline mb-0">{{ item.date }}</p>
                           <v-chip pill class="pa-6">
                             {{ item.content }}
                           </v-chip>
@@ -84,19 +89,22 @@
                 </v-list>
               </v-col>
             </v-row>
+          </v-card>
+          <v-card class="mt-16" outlined color="transparent">
             <v-row>
               <v-col>
-            <v-textarea
-              append-outer-icon="mdi-send"
-              @click:append-outer="send(activeUser)"
-              v-model="messageNew.text"
-              class="textArea mx-5 mb-5"
-              label="Mensagem"
-              rows="1"
-              outlined
-            ></v-textarea>
-            </v-col>
+                <v-textarea
+                  append-outer-icon="mdi-send"
+                  @click:append-outer="send(activeUser)"
+                  v-model="messageNew.text"
+                  class="textArea mx-5 mb-5"
+                  label="Mensagem"
+                  rows="1"
+                  outlined
+                ></v-textarea>
+              </v-col>
             </v-row>
+          </v-card>
           </v-card>
         </v-col>
       </v-row>
@@ -108,6 +116,8 @@
 <script>
 import axios from "axios";
 import store from "@/store/index.js";
+//mport VueOverflowScroll from 'vue-overflow-scroll'
+import moment from "moment";
 
 export default {
   data: () => ({
@@ -124,35 +134,37 @@ export default {
   components: {
     Bar: () => import("@/components/global/AppBarAccount.vue"),
     Foot: () => import("@/components/global/Footer"),
+    //VueOverflowScroll
   },
   methods: {
     send: async function (receiver) {
-        try {
-          console.log("Receiver: " + receiver);
-          console.log("Content: " + this.messageNew.text);
-          await axios.post("http://localhost:9040/message/addMessage", {
-            token: store.getters.token,
-            content: this.messageNew.text,
-            idUser2: receiver,
-          });
-            this.showChats();
-            this.messageNew.text=""
-           
-        } catch (e) {
-          this.$snackbar.showMessage({
-            show: true,
-            color: "warning",
-            text: "Ocorreu um erro, por favor tente mais tarde!",
-            timeout: 4000,
-          });
-        }
-
+      try {
+        console.log("Receiver: " + receiver);
+        console.log("Content: " + this.messageNew.text);
+        await axios.post("http://localhost:9040/message/addMessage", {
+          token: store.getters.token,
+          content: this.messageNew.text,
+          idUser2: receiver,
+        });
+        this.showChats();
+        this.messageNew.text = "";
+      } catch (e) {
+        this.$snackbar.showMessage({
+          show: true,
+          color: "warning",
+          text: "Ocorreu um erro, por favor tente mais tarde!",
+          timeout: 4000,
+        });
+      }
     },
 
     openChat(item) {
       this.activeUser = item.idUser;
       this.pic = item.image;
       this.showChats();
+    },
+    convertDate(d) {
+      return moment(d, moment.ISO_8601).format("DD-MM-YYYY");
     },
     showChats: async function () {
       try {
@@ -163,12 +175,12 @@ export default {
           }
         );
         this.users = response.data;
-        
+
         if (this.activeUser != 0) this.getMessage(this.activeUser);
-        else{
-          this.activeUser=response.data[0].idUser;
-        this.pic = response.data[0].image;
-         this.getMessage(this.users[0].idUser);
+        else {
+          this.activeUser = response.data[0].idUser;
+          this.pic = response.data[0].image;
+          this.getMessage(this.users[0].idUser);
         }
       } catch (e) {
         this.$snackbar.showMessage({
@@ -194,7 +206,7 @@ export default {
             idUser2: idReceiver,
           }
         );
-       
+        console.log(response);
         this.messages = response.data;
         if (this.messages[0].idReceive == idReceiver)
           this.idUser = this.messages[0].idGive;
@@ -216,16 +228,22 @@ export default {
 </script>
 
 <style scoped>
-.v-chip{
-  height: auto; 
+.v-chip {
+  height: auto;
   white-space: normal;
 }
 
-.textArea{
+.textArea {
   width: 95%;
   height: 50px;
   position: absolute;
-  bottom: 0; left: 0;
+  bottom: 0;
+  left: 0;
   resize: none;
+}
+
+.bCard{
+  display: flex;
+  flex-direction: column-reverse;
 }
 </style>
