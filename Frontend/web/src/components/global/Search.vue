79e9a112078ce206863be28447020c33b95a7288
13caroline <a80142@alunos.uminho.pt>
2022-02-06 14:15:18 +0000

    distance to exp

--- Frontend/web/src/components/global/Search.vue
+++ Frontend/web/src/components/global/Search.vue
@@ -2,98 +2,118 @@
   <v-col justify-centered cols="12" sm="12" md="12">
     <v-card flat>
       <div class="mx-sm-16 mx-md-0 mx-5 mx-lg-0 mx-xl-0">
-        <span>Pesquisar</span>
+        <p class="mt-6 mb-0 font-weight-bold">Pesquisar</p>
         <v-text-field
           outlined
           flat
           dense
-          class="rounded-xl mb-4"
           color="#78C4D4"
           v-model="formValues.search"
           append-icon="mdi-magnify"
         ></v-text-field>
       </div>
       <div class="mx-sm-16 mx-md-0 mx-5 mx-lg-0 mx-xl-0">
-        <span>Preço</span>
-        <v-slider
-          v-model="formValues.price"
-          inverse-label
-          :label="`${form.price} €`"
-          color="#78C4D4"
-          thumb-label
-          :max="`${form.price}`"
-          min="0"
-        ></v-slider>
+        <h4>Filtrar por:</h4>
       </div>
-      <div class="mx-sm-16 mx-md-0 mx-5 mx-lg-0 mx-xl-0">
-        <span>Distância</span>
-        <v-slider
-          v-model="formValues.distance"
-          inverse-label
-          :label="`5km`" 
-          color="#78C4D4"
-          thumb-label
-          :max="`${form.distance}`"
-          min="0"
-        ></v-slider>
-      </div>
-      <div class="mx-sm-16 mx-md-0 mx-5 mx-lg-0 mx-xl-0">
-        <span>Categoria</span>
-        <v-autocomplete
-          outlined
-          flat
-          class="rounded-xl"
-          dense
-          color="#78C4D4"
-          :items="form.cat"
-          item-value="idCategory"
-          item-text="name"
-          v-model="formValues.category"
-        />
-      </div>
-      <div class="mx-sm-16 mx-md-0 mx-5 mx-lg-0 mx-xl-0">
-        <span>Localização</span>
-        <v-autocomplete
-          outlined
-          flat
-          class="rounded-xl"
-          dense
-          color="#78C4D4"
-          :items="form.loc"
-          item-value="idLocation"
-          item-text="name"
-          v-model="formValues.location"
-        />
-      </div>
-      <div class="mx-sm-16 mx-md-0 mx-5 mx-lg-0 mx-xl-0">
-        <span>Classificação </span>
-        <v-slider
-          v-model="formValues.rating"
-          inverse-label
-          :label="`${formValues.rating}`"
-          color="#78C4D4"
-          thumb-label
-          :max="`${form.rating}`"
-          min="0"
-        ></v-slider>
-      </div>
-      <div class="mx-sm-16 mx-md-0 mx-5 mx-lg-0 mx-xl-0">
-        <span>Sexo</span>
-        <v-radio-group v-model="formValues.sex" row class="radio">
-          <v-radio
-            label="Feminino"
-            value="F"
-            color="#78C4D4"
-            class="ml-0"
-          ></v-radio>
-          <v-radio
-            label="Masculino"
-            value="M"
-            color="#78C4D4"
-            class="ml-0"
-          ></v-radio>
-        </v-radio-group>
-      </div>
+
+      <v-card class="mx-sm-16 mx-md-0 mx-5 mx-lg-0 mx-xl-0" outlined>
+        <v-list-item>
+          <v-list-item-content>
+            <div>
+              <p class="mt-2 mb-0 font-weight-bold">Preço</p>
+              <v-slider
+                v-model="formValues.price"
+                inverse-label
+                :label="`${form.price} €/hora`"
+                color="#78C4D4"
+                thumb-label
+                :max="`${form.price}`"
+                min="0"
+              ></v-slider>
+            </div>
+            <div>
+              <p class="mb-0 font-weight-bold">Experiência</p>
+              <v-slider
+                v-model="formValues.experience"
+                inverse-label
+                :label="`${form.experience} anos`"
+                color="#78C4D4"
+                thumb-label
+                :max="`${form.experience}`"
+                min="0"
+              ></v-slider>
+            </div>
+            <div>
+              <p class="mb-0 font-weight-bold">Classificação</p>
+              <v-slider
+                v-model="formValues.rating"
+                inverse-label
+                :label="`${form.rating}`"
+                color="#78C4D4"
+                thumb-label
+                :max="`${form.rating}`"
+                min="0"
+              ></v-slider>
+            </div>
+            <div>
+              <p class="mb-0 font-weight-bold">Localização</p>
+              <v-autocomplete
+                outlined
+                flat
+                dense
+                color="#78C4D4"
+                :items="form.loc"
+                item-value="idLocation"
+                item-text="name"
+                v-model="formValues.location"
+              />
+            </div>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+
+      <v-card class="mx-sm-16 mx-md-0 mx-5 mx-lg-0 mx-xl-0 mt-2" outlined>
+        <v-list-item>
+          <v-list-item-content>
+            <div>
+              <p class="font-weight-bold">Categoria</p>
+              <div v-for="c in form.cat" :key="c.idCategory">
+                <v-checkbox
+                  v-model="formValues.category"
+                  color="#78C4D4"
+                  :label="c.name"
+                  :value="c.idCategory"
+                  class="my-checkbox py-0 my-0"
+                ></v-checkbox>
+              </div>
+            </div>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+
+      <v-card class="mx-sm-16 mx-md-0 mx-5 mx-lg-0 mx-xl-0 mt-2" outlined>
+        <v-list-item>
+          <v-list-item-content>
+            <div>
+              <p class="mb-0 font-weight-bold">Sexo</p>
+              <v-checkbox
+                v-model="formValues.sex"
+                label="Feminino"
+                value="F"
+                color="#78C4D4"
+                class="my-checkbox"
+              ></v-checkbox>
+              <v-checkbox
+                v-model="formValues.sex"
+                label="Masculino"
+                value="M"
+                color="#78C4D4"
+                class="my-checkbox py-0 my-0"
+              ></v-checkbox>
+            </div>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn
@@ -126,26 +146,17 @@ export default {
       formValues: {
         search: null,
         category: null,
-        distance: null,
+        experience: null,
         price: null,
         rating: null,
         sex: null,
         location: null,
       },
       form: {
-        categories: [
-          "Companhia",
-          "Compras",
-          "Higiene",
-          "Medicação",
-          "Refeições",
-        ],
-        distance: 0,
+        experience: 0,
         price: "",
         search: "",
         rating: 0,
-        location: "",
-        row: null,
         loc: [],
         cat: [],
       },
@@ -163,10 +174,11 @@ export default {
   created: async function () {
     try {
       let response = await axios.get("http://localhost:9040/search/max");
+      console.log(response.data);
       if (response) {
         this.form.rating = response.data[0].rating;
         this.form.price = response.data[0].price;
-        this.form.distance = response.data[0].distance;
+        this.form.experience = response.data[0].experience;
       }
     } catch (e) {
       console.log(e);
@@ -194,12 +206,16 @@ export default {
 </script>
 
 <style scoped>
-span {
+p {
   color: #797878;
   font-size: small;
 }
 
-.radio {
-  font-size: 10px;
+h4 {
+  color: #707978;
+}
+
+::v-deep .my-checkbox .v-label {
+  font-size: 15px;
 }
 </style>
