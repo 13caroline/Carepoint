a135cd81c9efcb2d77c3c1bf4ac5b27a9c80bcca
Rodolfo Silva <35670736+Th0l@users.noreply.github.com>
2022-02-07 11:43:10 +0000

    Update all_queries.sql

--- Database/sql/all_queries.sql
+++ Database/sql/all_queries.sql
@@ -1867,7 +1867,7 @@ BEGIN
 		CAST(REPLACE(json_extract(j1, '$.idCategory'),'"','') AS JSON) AS categories, json_extract(j1, '$.date_requested') AS date_requested, CAST(REPLACE(CONCAT('[',
          IF (json_extract(j1, '$.idCategory') LIKE '%1%', '"Apoio externo",',''),IF (json_extract(j1, '$.idCategory') LIKE '%2%', '"Cuidados de higiene e conforto pessoal",',''),
          IF (json_extract(j1, '$.idCategory') LIKE '%3%', '"Cuidados de lazer",',''),IF (json_extract(j1, '$.idCategory') LIKE '%4%', '"Cuidados médicos",',''),
-         IF (json_extract(j1, '$.idCategory') LIKE '%5%', '"Fornecimento e apoio nas refeições",','') ,IF (json_extract(j1, '$.idCategory') LIKE '%6%', '"Higiene habitacional"',''),']'),',]',']') AS JSON) AS array_categories from json_table(@json_bug, '$[*]' columns ( j1 json path '$')) as jt 
+         IF (json_extract(j1, '$.idCategory') LIKE '%5%', '"Fornecimento e apoio nas refeições",','') ,IF (json_extract(j1, '$.idCategory') LIKE '%6%', '"Higiene habitacional"',''),']'),',]',']') AS JSON) AS array_categories from json_table(os, '$[*]' columns ( j1 json path '$')) as jt 
 		inner join user ON CAST(json_extract(j1, '$.id') AS UNSIGNED) = user.idUser
 		where CAST(json_extract(j1, '$.occupied') AS UNSIGNED) = 0;
 
